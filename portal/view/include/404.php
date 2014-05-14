904e417166d7823d7ff0b357af92c1599ca98897
pengshen001 <peng.shen@indochino.com>
2014-05-13 21:49:41 -0700

    add header links

--- portal/view/include/404.php
+++ portal/view/include/404.php
@@ -1,3 +1,8 @@
-<?php 
+<?php
+
+include 'view/include/header.php';
 ?>
-404 Page Not Found
+404 Page Not Found.
+<?php 
+include 'view/include/footer.php';
+?>
--- portal/view/include/header.php
+++ portal/view/include/header.php
@@ -1,17 +1,19 @@
 <?php 
-global $base_host; 
+global $base_host, $account_url; 
 ?><!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?=isset($title) ? $title : '' ?></title>
+<title><?=isset($title) ? $title :'' ?></title>
 <link rel="shortcut icon" href="/portal/img/favicon.ico" type="image/x-icon">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
 <script src="/portal/js/common.js"></script>
+<script src="/portal/js/account.js"></script>
 
 <link rel="stylesheet" href="/portal/css/common.css">
+<link rel="stylesheet" href="/portal/css/account.css">
 <?php if (isset($stylesheets)) {
     foreach ($stylesheets as $stylesheet) {
         echo '<link rel="stylesheet" href="/portal/css/'.$stylesheet.'">';
@@ -21,6 +23,7 @@ global $base_host;
 <body>
 <div id="header">
 <div id="header_inner">
+<div id="profile"></div>
 <a id="logo" href="<?php global $www_url; echo $www_url; ?>">con<span>fone</span></a>
 </div>
 </div>
