f9826310d49700d5bfafcd612bb868af23ff1f54
pengshen001 <peng.shen@indochino.com>
2014-05-16 22:33:05 -0700

    default redirect

--- portal/controller/page/SecurityHomeController.php
+++ portal/controller/page/SecurityHomeController.php
@@ -2,6 +2,7 @@
 class SecurityHomeController extends ViewController {
 
 	protected function control() {
+		$this->redirect('/application/list');
 		$this->render(array(
 			'view' => 'page/home.php'
 		));
